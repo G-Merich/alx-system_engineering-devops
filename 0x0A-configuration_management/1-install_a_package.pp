50348bf8275a1b2ca16953115b49e917ce35ccfb
Gloria Merit Ogudu <gmerit2030@gmail.com>
2023-04-24 13:40:06 -0700

    intall

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,7 @@
 # INSTALL : 'puppet-lint'
 
-package { 'puppet-lint':
-	ensure   => '2.1.0',
-	provider => 'flask'
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
+
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,6 @@
 # EXECUTE : 'pkill'
-exe { 'pkill':
-	command => 'pkill -9 -f killmenow',
-	path    => ['/usr/bin', '/usr/sbin', '/bin']
+
+exec { 'killmenow':
+  command => 'pkill killmenow',
+  path    => '/usr/bin/'
 }
